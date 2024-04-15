a1d6d3ac99e8ea18f9ad0debf164a54f452f42c1
Bart Massey <bart.massey@gmail.com>
2024-04-15 00:51:13 -0700

    added 404 fallback

--- src/main.rs
+++ src/main.rs
@@ -21,9 +21,13 @@ async fn jokes() -> Response {
     (StatusCode::OK, Json(jokebase)).into_response()
 }
 
+async fn handler_404() -> Response {
+    (StatusCode::NOT_FOUND, "404 Not Found").into_response()
+}
+
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/jokes", get(jokes));
+    let app = Router::new().route("/jokes", get(jokes)).fallback(handler_404);
     let ip = SocketAddr::new([127, 0, 0, 1].into(), 3000);
     eprintln!("knock-knock: serving {}", ip);
     let listener = tokio::net::TcpListener::bind(ip).await.unwrap();
