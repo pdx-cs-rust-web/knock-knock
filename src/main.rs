d4e0176b133285a6b0cec46d5ad58f876740a110
Bart Massey <bart.massey@gmail.com>
2024-04-15 01:13:53 -0700

    added tracing

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,10 @@ use axum::{
     routing::get,
     Json, Router,
 };
+extern crate tokio;
+use tower_http::trace;
+extern crate tracing;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 async fn jokes() -> Response {
     let jokebase = vec![Joke::new(
@@ -27,9 +31,25 @@ async fn handler_404() -> Response {
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/jokes", get(jokes)).fallback(handler_404);
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| "knock_knock=debug,info".into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+    // https://carlosmv.hashnode.dev/adding-logging-and-tracing-to-an-axum-app-rust
+    let trace_layer = trace::TraceLayer::new_for_http()
+        .make_span_with(trace::DefaultMakeSpan::new().level(tracing::Level::INFO))
+        .on_response(trace::DefaultOnResponse::new().level(tracing::Level::INFO));
+
+    let app = Router::new()
+        .route("/jokes", get(jokes))
+        .fallback(handler_404)
+        .layer(trace_layer);
+
     let ip = SocketAddr::new([127, 0, 0, 1].into(), 3000);
-    eprintln!("knock-knock: serving {}", ip);
     let listener = tokio::net::TcpListener::bind(ip).await.unwrap();
+    tracing::debug!("serving {}", listener.local_addr().unwrap());
     axum::serve(listener, app).await.unwrap();
 }
