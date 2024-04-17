c75b10ea4f949c0d37413fc187f312a65b8696b4
Bart Massey <bart.massey@gmail.com>
2024-04-16 23:00:55 -0700

    cleaned up joke adding

--- add-jokes.sh
+++ add-jokes.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-for F in assets/jokes/*/jokes/*.json
+for F in assets/jokes/*.json
 do
     curl -d "@$F" -H "Content-Type: application/json" -X POST http://localhost:3000/api/v1/joke/add
 done
