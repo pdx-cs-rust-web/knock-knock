2e2377ba5b010ca7dcc6a906918e0200a8459725
Bart Massey <bart@cs.pdx.edu>
2024-04-22 15:50:51 -0700

    added CSS to templating

--- src/web.rs
+++ src/web.rs
@@ -4,11 +4,21 @@ use crate::*;
 #[template(path = "index.html")]
 pub struct IndexTemplate<'a> {
     joke: &'a Joke,
+    stylesheet: &'static str,
+}
+
+impl<'a> IndexTemplate<'a> {
+    fn new(joke: &'a Joke) -> Self {
+        Self {
+            joke,
+            stylesheet: "/knock-knock.css",
+        }
+    }
 }
 
 pub async fn handler_index(State(jokebase): State<Arc<RwLock<JokeBase>>>) -> Response {
     match jokebase.read().await.get_random() {
-        Some(joke) => (StatusCode::OK, IndexTemplate { joke }).into_response(),
+        Some(joke) => (StatusCode::OK, IndexTemplate::new(joke)).into_response(),
         None => (StatusCode::NOT_FOUND, "404 Not Found").into_response(),
     }
 }
