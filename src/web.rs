a7208610fe0cc2e4a7b9ae2722050ab7ffee40d9
Bart Massey <bart.massey@gmail.com>
2024-04-24 15:54:30 -0700

    added tell functionality to website

--- src/web.rs
+++ src/web.rs
@@ -38,3 +38,60 @@ pub async fn handler_index(
         Err(e) => (StatusCode::NO_CONTENT, e.to_string()).into_response(),
     }
 }
+
+#[derive(Template)]
+#[template(path = "tell.html")]
+pub struct TellTemplate {
+    stylesheet: &'static str,
+}
+
+impl TellTemplate {
+    fn new() -> Self {
+        Self { stylesheet: "/knock-knock.css" }
+    }
+}
+
+pub async fn handler_tell() -> Response {
+    (StatusCode::OK, TellTemplate::new()).into_response()
+}
+
+#[derive(Deserialize)]
+pub struct AddParams {
+    id: String,
+    who: String,
+    answer: String,
+    tags: Option<String>,
+    source: Option<String>,
+}
+
+fn parse_tags(tags: Option<String>) -> Option<HashSet<String>> {
+    tags.map(|tags| {
+        tags.split(',').map(str::trim).map(str::to_string).collect()
+    })
+}
+
+pub async fn handler_add(
+    State(jokebase): State<Arc<RwLock<JokeBase>>>,
+    Query(params): Query<AddParams>,
+) -> Response {
+    // XXX Condition user input.
+    let joke = Joke {
+        id: params.id,
+        whos_there: params.who,
+        answer_who: params.answer,
+        tags: parse_tags(params.tags),
+        source: params.source,
+    };
+
+    let mut jokebase = jokebase.write().await;
+
+    match jokebase.add(joke) {
+        Ok(()) => (StatusCode::OK, "".to_string()).into_response(),
+        Err(JokeBaseErr::JokeBaseIoError(msg)) =>
+            (StatusCode::INTERNAL_SERVER_ERROR, msg).into_response(),
+        Err(JokeBaseErr::JokeExists(id)) =>
+            (StatusCode::CONFLICT, id).into_response(),
+        Err(e) =>
+            (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()).into_response(),
+    }
+}
