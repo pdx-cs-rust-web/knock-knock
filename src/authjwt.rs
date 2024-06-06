b24c75cb016aa00a73ac6717179c9764091ffaf2
Bart Massey <bart.massey@gmail.com>
2024-06-05 18:15:41 -0700

    got jwt authorization fully working

--- src/authjwt.rs
+++ src/authjwt.rs
@@ -82,7 +82,7 @@ impl FromRequestParts<SharedAppState> for Claims {
     type Rejection = AuthError;
 
     async fn from_request_parts(parts: &mut Parts, state: &SharedAppState) -> Result<Self, Self::Rejection> {
-        use jsonwebtoken::{Validation, decode};
+        use jsonwebtoken::{Algorithm, Validation, decode};
 
         // Extract the token from the authorization header
         let TypedHeader(Authorization(bearer)) = parts
@@ -90,9 +90,15 @@ impl FromRequestParts<SharedAppState> for Claims {
             .await
             .map_err(|_| AuthError::InvalidToken)?;
         // Decode the user data
-        let token_data = decode::<Claims>(bearer.token(), &state.read().await.jwt_keys.decoding, &Validation::default())
-            .map_err(|_| AuthError::InvalidToken)?;
-
+        let appstate = state.read().await;
+        let decoding_key = &appstate.jwt_keys.decoding;
+        let validation = Validation::new(Algorithm::HS512);
+        let result = decode::<Claims>(
+            bearer.token(),
+            decoding_key,
+            &validation,
+        );
+        let token_data = result.map_err(|_| AuthError::Registration)?;
         Ok(token_data.claims)
     }
 }
@@ -102,7 +108,7 @@ impl IntoResponse for AuthError {
         let (status, error_message) = match self {
             AuthError::Registration => (StatusCode::UNAUTHORIZED, "Invalid registration"),
             AuthError::TokenCreation => (StatusCode::INTERNAL_SERVER_ERROR, "Token creation error"),
-            AuthError::InvalidToken => (StatusCode::BAD_REQUEST, "Invalid token"),
+            AuthError::InvalidToken => (StatusCode::UNAUTHORIZED, "Invalid token"),
         };
         let body = Json(serde_json::json!({
             "status": status.as_u16(),
@@ -128,8 +134,8 @@ pub struct Claims {
     iss: String,
     #[schema(example = "John Smith <johnsmith@example.org>")]
     sub: String,
-    #[schema(format = "DateTime", example = "2024-06-04T21:49:06Z")]
-    exp: String,
+    #[schema(example = "1717630066")]
+    exp: u64,
 }
 
 pub fn make_jwt_token(appstate: &AppState, registration: &Registration) -> Result<AuthBody, AuthError> {
@@ -141,8 +147,8 @@ pub fn make_jwt_token(appstate: &AppState, registration: &Registration) -> Resul
 
     let iss = "knock-knock.po8.org".to_string();
     let sub = format!("{} <{}>", registration.full_name, registration.email);
-    let exp = (Utc::now() + TimeDelta::days(1))
-        .to_rfc3339_opts(SecondsFormat::Secs, true);
+    let exp = (Utc::now() + TimeDelta::days(1)).timestamp();
+    let exp = u64::try_from(exp).unwrap();
     let claims = Claims { iss, sub, exp };
     let header = Header::new(Algorithm::HS512);
     let token = encode(&header, &claims, &appstate.jwt_keys.encoding)
